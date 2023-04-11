f14cff4ffce120c80bca9f43d4ce22d31918c8fc
Jacob J <96319709+night780@users.noreply.github.com>
2023-04-11 10:29:49 -0700

    Update CodeSnippetLibrary.java

--- CodeSnippet/src/main/java/CodeSnippetLibrary.java
+++ CodeSnippet/src/main/java/CodeSnippetLibrary.java
@@ -1,318 +1,187 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 
 /**
- * Code Snippet Library GUI, a program that allows the user to
- * create, view, and add to code snippet libraries.
- *
+ * The type Code snippet library.
  * @author Jacob Jonas
  * @version 1.0
  * @date 4-10-23
  */
-public class CodeSnippetLibraryGUI extends JFrame implements ActionListener {
+public class CodeSnippetLibrary extends Component {
     private static final String ROOT_PATH = "./";
     private static final String FILE_EXTENSION = ".txt";
-    private JLabel label;
-    private JTextField textField;
-    private JTextArea textArea;
-    private JButton viewButton, createButton, addButton;
-
-
-
-    /**
-     * The CodeSnippetLibraryGUI function creates a GUI for the Code Snippet Library program.
-     * It contains three buttons: View, Create, and Add. The View button allows the user to view existing libraries;
-     * the Create button allows them to create a new library; and the Add button allows them to add code snippets
-     * from an existing library into another one. Each of these buttons has an icon associated with it that is displayed
-     * on top of it instead of text (this makes for a more visually appealing GUI). When each button is clicked, its corresponding function will be executed.
-
-     *
-     *
-     * @return Nothing
-     *
-     */
-    public CodeSnippetLibraryGUI() {
-
-
-        super("Code Snippet Library");
-        setSize(500, 500);
-        //set icon
-        try {
-            setIconImage(ImageIO.read(new File("icon.png")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        setBackground(new Color(135, 145, 145)); // Use a consistent color scheme throughout the GUI.
-        JPanel topPanel = new JPanel();
-        topPanel.setBackground(new Color(135, 145, 145));
-        topPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        //set background color for bottom panel
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setBackground(new Color(135, 145, 145));
-
-        ImageIcon logoIcon = new ImageIcon("logo.png"); // Use a title bar image to give the GUI a professional look.
-        JLabel logoLabel = new JLabel(logoIcon);
-        topPanel.add(logoLabel);
-
-        label = new JLabel("Code Snippet Library Menu");
-        label.setFont(new Font("Arial", Font.BOLD, 18)); // Increase the font size and use a clear and readable font.
-        topPanel.add(label);
-
-        add(topPanel, BorderLayout.NORTH);
-
-        JPanel centerPanel = new JPanel();
-        centerPanel.setBackground(new Color(135, 145, 145));
-        centerPanel.setLayout(new GridLayout(3, 1, 10, 10));
-
-        viewButton = new JButton(new ImageIcon("view.png")); // Use icons or images instead of text for buttons.
-        viewButton.setIcon(new ImageIcon(((ImageIcon) viewButton.getIcon()).getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
-        viewButton.setToolTipText("View existing libraries");
-        viewButton.addActionListener(this);
-        viewButton.setBackground(new Color(165, 199, 199, 255));
-
-        centerPanel.add(viewButton);
-
-        createButton = new JButton(new ImageIcon("create.png"));
-        createButton.setIcon(new ImageIcon(((ImageIcon) createButton.getIcon()).getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
-        createButton.setToolTipText("Create a new library");
-        createButton.addActionListener(this);
-        createButton.setBackground(new Color(165, 199, 199, 255));
-        centerPanel.add(createButton);
-
-        addButton = new JButton(new ImageIcon("add.png"));
-        //make the icon smaller
-        addButton.setIcon(new ImageIcon(((ImageIcon) addButton.getIcon()).getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
-        addButton.setToolTipText("Add code to a library");
-        addButton.addActionListener(this);
-        addButton.setBackground(new Color(165, 199, 199, 255));
-
-        centerPanel.add(addButton);
-
-        add(centerPanel, BorderLayout.CENTER);
-
-
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBackground(new Color(135, 145, 145));
-        add(scrollPane, BorderLayout.SOUTH);
-
-        setVisible(true);
-    }
-
-
 
 
     /**
      * The main function of the program.
      *
      *
-     * @param args Pass arguments to the program
+     * @param args Pass command line arguments to the program
      *
      * @return Void
      *
      */
-    public static void main(String[] args) {
-        new CodeSnippetLibraryGUI();
-    }
+    public void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    /**
-     * The actionPerformed function is the function that is called when a button
-     * on the GUI is pressed. It determines which button was pressed and calls
-     * the appropriate function to handle it.
+        while (true) {
+            System.out.println("Code Snippet Library Menu:");
+            System.out.println("1. View existing libraries");
+            System.out.println("2. Create a new library");
+            System.out.println("3. Add code to a library");
+            System.out.println("4. Exit");
 
-     *
-     * @param e Determine which button was pressed
-     *
-     * @return Void
-     */
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == viewButton) {
-            viewExistingLibraries();
-        } else if (e.getSource() == createButton) {
-            createNewLibrary();
-        } else if (e.getSource() == addButton) {
-            addCodeToLibrary(this);
+            System.out.print("Enter your choice (1-4): ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume the newline character
+
+            switch (choice) {
+                case 1:
+                    viewExistingLibraries();
+                    break;
+                case 2:
+                    createNewLibrary(scanner);
+                    break;
+                case 3:
+                    addCodeToLibrary(scanner);
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice!");
+            }
         }
     }
+
 
 
     /**
      * The viewExistingLibraries function allows the user to view the contents of a library.
      * The function first checks if there are any libraries in existence, and if not, displays an error message.
-     * If there are libraries in existence, it prompts the user to choose one from a list of existing libraries.
-     * Once chosen, it opens up a new window with that library's content displayed inside of it.  It also adds a window listener so that when this new window is closed by clicking on its &quot;X&quot; button or otherwise closing itself out (e.g., via Alt+F4), then the program will prompt you to save changes before
+     * If there are libraries in existence, it prompts the user to enter which library they would like to view.
+     * It then reads that file's contents and displays them for the user using a JTextArea object within a JFrame window.
+
      *
      *
-     * @return A string of the file name
+     * @return The contents of a file
      *
      */
     public void viewExistingLibraries() {
         File root = new File(ROOT_PATH);
         File[] files = root.listFiles((dir, name) -> name.endsWith(FILE_EXTENSION));
+        JTextArea textArea = null;
         if (files == null || files.length == 0) {
-            JOptionPane.showMessageDialog(this, "No libraries found.");
+            textArea.setText("No libraries found.");
         } else {
             StringBuilder sb = new StringBuilder();
             sb.append("Existing libraries:\n");
-            // prompt user to choose a library to view
-            String[] options = new String[files.length];
-            for (int i = 0; i < files.length; i++) {
-                options[i] = files[i].getName();
+            for (File file : files) {
+                sb.append(file.getName() + "\n");
             }
-            String libraryName = (String) JOptionPane.showInputDialog(this, sb.toString(), "View Library",
-                    JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+            // prompt user to view contents of a library
+            String libraryName = JOptionPane.showInputDialog(this, sb.toString() + "\nEnter the name of the library to view:");
             if (libraryName != null && !libraryName.isEmpty()) {
-                String filePath = ROOT_PATH + libraryName;
-                try {
+                String fileContent = CodeSnippetLibrary.getFileContents(libraryName + FILE_EXTENSION);
+                if (fileContent != null) {
                     JFrame frame = new JFrame("Library Content: " + libraryName);
-                    JTextArea textArea = new JTextArea();
+                    textArea = new JTextArea(fileContent);
                     JScrollPane scrollPane = new JScrollPane(textArea);
                     frame.add(scrollPane);
                     frame.setSize(500, 500);
                     frame.setVisible(true);
-                    // load file content into the text area
-                    String fileContent = CodeSnippetLibrary.getFileContents(libraryName);
-                    if (fileContent != null) {
-                        textArea.setText(fileContent);
-                    } else {
-                        JOptionPane.showMessageDialog(this, "Failed to read file: " + libraryName + FILE_EXTENSION);
-                    }
-
-                    // add window listener to prompt user to save changes before closing
-                    frame.addWindowListener(new WindowAdapter() {
-                        @Override
-                        public void windowClosing(WindowEvent e) {
-                            //if no changes were made, then just close the window
-                            if (textArea.getText().equals(fileContent)) {
-                                frame.dispose();
-                                return;
-                            }
-                            int choice = JOptionPane.showConfirmDialog(frame, "Save changes?", "Save Changes",
-                                    JOptionPane.YES_NO_CANCEL_OPTION);
-                            if (choice == JOptionPane.YES_OPTION) {
-                                String content = textArea.getText();
-                                try (PrintWriter out = new PrintWriter(filePath)) {
-                                    out.println(content);
-                                    JOptionPane.showMessageDialog(frame, "Changes saved successfully.");
-                                } catch (FileNotFoundException ex) {
-                                    JOptionPane.showMessageDialog(frame, "Failed to save changes: " + ex.getMessage());
-                                }
-                            } else if (choice == JOptionPane.NO_OPTION) {
-                                // do nothing
-                            } else {
-                                // user cancelled
-                                return;
-                            }
-                            frame.dispose();
-                        }
-                    });
-                } catch (Exception e) {
-                    JOptionPane.showMessageDialog(this, "Failed to open file: " + libraryName + FILE_EXTENSION);
-                }
-            }
-        }
-    }
-
-
-
-
-
-    /**
-     * The createNewLibrary function creates a new library file.
-     * The user is prompted to enter the name of the new library, and if they do not cancel out of this prompt,
-     * then a file with that name is created in the root directory. If there already exists a file with that name,
-     * then an error message will be displayed to inform them of this fact. Otherwise, it will display a success message
-
-     *
-     *
-     * @return Nothing
-     *
-     */
-    public void createNewLibrary() {
-
-        String libraryName = JOptionPane.showInputDialog(this, "Enter the name of the new library:");
-        if (libraryName != null && !libraryName.isEmpty()) {
-            String fileName = libraryName + FILE_EXTENSION;
-            File file = new File(ROOT_PATH + fileName);
-            try {
-                if (file.createNewFile()) {
-                    JOptionPane.showMessageDialog(this, "New library created: " + fileName);
                 } else {
-                    JOptionPane.showMessageDialog(this, "Library already exists: " + fileName);
+                    JOptionPane.showMessageDialog(this, "Failed to read file: " + libraryName + FILE_EXTENSION);
                 }
-            } catch (IOException e) {
-                JOptionPane.showMessageDialog(this, "Failed to create library: " + fileName);
-                e.printStackTrace();
             }
         }
     }
 
 
-    /**
-     * The addCodeToLibrary function allows the user to add code to a library.
-     * The function first prompts the user for a library name, and then checks if that file exists.
-     * If it does not exist, an error message is displayed and the function returns.
-     * Otherwise, another prompt asks for code to be added to that library file.
 
+    /**
+     * The createNewLibrary function creates a new library file with the name specified by the user.
      *
-     * @param mainFrame Display the joptionpane dialogs
      *
-     * @return Void
+     * @param scanner Read the user input
+     *
+     * @return Void, so the return type should be void
      *
      */
-    private static void addCodeToLibrary(JFrame mainFrame) {
-        File root = new File(ROOT_PATH);
-        File[] files = root.listFiles((dir, name) -> name.endsWith(FILE_EXTENSION));
-        if (files == null || files.length == 0) {
-            JOptionPane.showMessageDialog(mainFrame, "No libraries found.");
-            return;
-        }
-
-        // prompt user to choose a library to add code to
-        String[] options = new String[files.length];
-        for (int i = 0; i < files.length; i++) {
-            options[i] = files[i].getName().replace(FILE_EXTENSION, "");
-        }
-        String libraryName = (String) JOptionPane.showInputDialog(mainFrame, "Select a library to add code to:",
-                "Add Code to Library", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-        if (libraryName == null || libraryName.isEmpty()) {
-            return;
-        }
+    private static void createNewLibrary(Scanner scanner) {
+        System.out.print("Enter the name of the new library: ");
+        String libraryName = scanner.nextLine();
         String fileName = libraryName + FILE_EXTENSION;
         File file = new File(ROOT_PATH + fileName);
-        if (!file.exists()) {
-            JOptionPane.showMessageDialog(mainFrame, "Library not found: " + fileName, "Error", JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-
-        // prompt user to enter code to add to library
-        String code = JOptionPane.showInputDialog(mainFrame, "Enter the code to add:", "Add Code to Library", JOptionPane.PLAIN_MESSAGE);
-        if (code == null || code.isEmpty()) {
-            return;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < code.length(); i += 58) {
-            int endIndex = Math.min(i + 58, code.length());
-            String line = code.substring(i, endIndex) + "\n";
-            sb.append(line);
-        }
-        sb.append("----------------------------------------------------------------\n");
-        try (FileWriter writer = new FileWriter(file, true)) {
-            writer.write(sb.toString());
-            JOptionPane.showMessageDialog(mainFrame, "Code added to library: " + fileName, "Success", JOptionPane.INFORMATION_MESSAGE);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("New library created: " + fileName);
+            } else {
+                System.out.println("Library already exists: " + fileName);
+            }
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(mainFrame, "Failed to add code to library: " + fileName, "Error", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Failed to create library: " + fileName);
             e.printStackTrace();
         }
     }
 
+    /**
+     * The addCodeToLibrary function allows the user to add code to an existing library.
+     * The function prompts the user for a library name, and then checks if that file exists.
+     * If it does not exist, it prints out an error message and returns from the function.
+     * Otherwise, it opens up a FileWriter object in append mode (so that we don't overwrite any existing code)
+     * and writes whatever string is entered by the user into this file on its own line.
 
+     *
+     * @param scanner Get user input from the console
+     *
+     * @return Nothing
+     *
+     */
+    private static void addCodeToLibrary(Scanner scanner) {
+        System.out.print("Enter the name of the library to add code to: ");
+        String libraryName = scanner.nextLine();
+        String fileName = libraryName + FILE_EXTENSION;
+        File file = new File(ROOT_PATH + fileName);
+        if (!file.exists()) {
+            System.out.println("Library not found: " + fileName);
+            return;
+        }
+        try (FileWriter writer = new FileWriter(file, true)) {
+            System.out.print("Enter the code to add: ");
+            String code = scanner.nextLine();
+            writer.write(code + "\n");
+            System.out.println("Code added to library: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Failed to add code to library: " + fileName);
+            e.printStackTrace();
+        }
+    }
 
+    public static String getFileContents(String fileName) {
+        File file = new File(ROOT_PATH + fileName);
+        if (!file.exists()) {
+            return "File not found: " + fileName;
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to read file: " + fileName;
+        }
+    }
 
 }
